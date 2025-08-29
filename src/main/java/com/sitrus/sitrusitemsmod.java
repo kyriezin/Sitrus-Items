022e0f6e1df21896c4990e6d05fd21033507cf3d
kyriezin <kyriezeroum01@gmail.com>
2025-08-29 12:48:47 -0300

    kyrie's Commit

---
+++ src/main/java/com/sitrus/sitrusitemsmod.java
@@ -0,0 +1,13 @@
+package com.sitrus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+	public static final String MOD_ID = "sitrusitems";
+
+	@Override
+	public void onInitialize() {
+		// Código de inicialização do mod
+		System.out.println("Sitrus Items carregado!");
+	}
+}
