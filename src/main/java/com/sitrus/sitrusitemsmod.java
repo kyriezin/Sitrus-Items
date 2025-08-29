22a2abc2a712461ef4fc8e7d6b08d58d30150322
kyriezin <kyriezeroum01@gmail.com>
2025-08-29 12:59:33 -0300

    kyrie's Commit

--- src/main/java/com/sitrus/sitrusitemsmod.java
+++ src/main/java/com/sitrus/sitrusitemsmod.java
@@ -1,13 +1,19 @@
 package com.sitrus;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
-public class ExampleMod implements ModInitializer {
+public class SitrusItems implements ModInitializer {
 	public static final String MOD_ID = "sitrusitems";
+
+	// Item "Chave"
+	public static final Item CHAVE = new Item(new Item.Settings());
 
 	@Override
 	public void onInitialize() {
-		// Código de inicialização do mod
-		System.out.println("Sitrus Items carregado!");
+		// Registrar a chave no jogo
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "chave"), CHAVE);
 	}
 }
